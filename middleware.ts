e91915856273cd309b9811d76bc438f457d473a5
saasapp-rd <saasapp@seattleacademy.org>
2026-05-01 11:38:10 -0700

    feat: add /missing to protected route middleware

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,14 @@ export { default } from "next-auth/middleware"
 export const config = {
   matcher: [
     "/dashboard/:path*",
+    "/missing/:path*",
     "/teacher/:path*",
     "/coordinator/:path*",
     "/counselor/:path*",
     "/dean/:path*",
     "/admin/:path*",
     "/staff/:path*",
+    "/student/:path*",
     "/design-lab/:path*",
   ],
 }
